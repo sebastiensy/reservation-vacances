0f5ec7f954253a7bf3a2edf68baf2dd91f13c5f5
sebastiensy <seb6890@gmail.com>
2015-09-29 21:21:12 +0200

    Premier commit

---
+++ reserv-vac/src/main/java/fr/uha/miage/reservac/ReservVacApplication.java
@@ -0,0 +1,12 @@
+package fr.uha.miage.reservac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservVacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservVacApplication.class, args);
+    }
+}
---
+++ reserv-vac/src/test/java/fr/uha/miage/reservac/ReservVacApplicationTests.java
@@ -0,0 +1,18 @@
+package fr.uha.miage.reservac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReservVacApplication.class)
+@WebAppConfiguration
+public class ReservVacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
