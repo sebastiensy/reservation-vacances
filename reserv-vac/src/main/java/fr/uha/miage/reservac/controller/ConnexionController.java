0581e9ee4e0c53e709687e4bcfa3a692de18e1e9
sebastiensy <seb6890@gmail.com>
2015-09-30 11:15:03 +0200

    Commit

---
+++ reserv-vac/src/main/java/fr/uha/miage/reservac/controller/ConnexionController.java
@@ -0,0 +1,18 @@
+package fr.uha.miage.reservac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.uha.miage.reservac.model.Utilisateur;
+
+@Controller
+public class ConnexionController {
+
+	/*@RequestMapping("/connexion")
+	public String requestCreatePage(Model model) {
+		
+		model.addAttribute("user", new Utilisateur());
+		return "connexion";
+	}*/
+}
